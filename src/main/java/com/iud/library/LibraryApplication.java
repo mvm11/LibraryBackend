5e1ae6752b7acb7d8596312a2930d92744eb400c
Mateo <mvalenciaminota@gmail.com>
2022-10-18 23:22:33 -0500

    initial commit

---
+++ src/main/java/com/iud/library/LibraryApplication.java
@@ -0,0 +1,13 @@
+package com.iud.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryApplication.class, args);
+	}
+
+}
