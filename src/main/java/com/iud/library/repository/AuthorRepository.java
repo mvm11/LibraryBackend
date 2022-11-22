84a5cf4b256109ff5007266019aa240fa1b298ec
Mateo <mvalenciaminota@gmail.com>
2022-11-21 23:42:24 -0500

    Author crud added

--- src/main/java/com/iud/library/repository/AuthorRepository.java
+++ src/main/java/com/iud/library/repository/AuthorRepository.java
@@ -4,6 +4,10 @@ import com.iud.library.entity.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AuthorRepository extends JpaRepository<Author, Integer> {
+
+    public List<Author> findByBookId(Integer bookId);
 }
